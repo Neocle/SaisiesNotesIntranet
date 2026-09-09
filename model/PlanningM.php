06711e59b0b456dbe92b1882f909b11c94a352ef
Alexandre <firefox42c@gmail.com>
2026-09-09 12:20:51 +0200

    Model PlanningM.php ajouté plus fonction getPlanningEnseignant

--- model/PlanningM.php
+++ model/PlanningM.php
@@ -1,5 +1,21 @@
 <?php
 
-function getPlanningEnseignants(!idEnseignant){
-    
+public function getPlanningEnseignants($idEnseignant){
+    $sql = "SELECT *
+            FROM livres
+            WHERE titre  LIKE :recherche
+               OR auteur LIKE :recherche
+               OR resumer LIKE :recherche
+               OR genre LIKE :recherche";
+
+    $stmt = $pdo->prepare($sql);
+
+    $search = "%" . $recherche . "%";
+
+    $stmt->execute([
+        ':recherche' => $search
+    ]);
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 }
