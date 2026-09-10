ac887edd5870a7ec486385de6dcf62a240da487a
vduprat <v.duprat63@gmail.com>
2026-09-10 11:41:45 +0200

    Controller et vue planning

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
 require_once __DIR__ . '/database/DatabaseConnection.php';
+require_once 'controller/PlanningController.php';
+
+$planningController = new planningController();
+
+$action = $_GET['action'] ?? 'planning';
+
+switch($action) {
+    case 'planning':
+        $planningController->getPlanningEnseignants();
+        break;    
+    default: 
+}
