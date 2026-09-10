1bc86b5d18f06503279de77a43ac30953c216196
pierre799 <pierreguillotdel@gmail.com>
2026-09-10 11:56:01 +0200

    jschaispas

--- view/grilleEval/show.php
+++ view/grilleEval/show.php
@@ -1,16 +1,21 @@
 <!--AFFICHAGE DU TABLEAU DES CRITERES-->
 
 <?php include 'view/layout/header.php'; ?>
-<link rel="stylesheet" type="text/css" href="css/grilleEval.css">
+<link rel="stylesheet" type="text/css" href="public/css/grilleEval.css">
 
 <h2>TABLEAU DES CRITERES</h2>
 <table>
 <!--AJOUT D'UNE LIGNE POUR CHAQUE $c DANS LE TABLEAU $critereseval-->
 <?php foreach ($critereseval as $c): ?>
     <tr>
-        <?= $c['descLongue'] ?> ---- <?= $c['descCourte'] ?>
+        <td>Section</td>
+        <td><?= $c['descLongue'] ?></td>
+        <td><?= $c['descCourte'] ?></td>
+        <td><textarea></textarea></td>
     </tr>
 <?php endforeach; ?>
 </table>
+
+<div></div>
 
 <!-- http://localhost/SaisiesNotesIntranet/view/grilleEval/show.php -->
